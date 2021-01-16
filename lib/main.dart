07d82299ce8a07e2747e3141d6d71ec22f5a206f
Yash <ydeshmukh0093@gmail.com>
2021-01-16 15:39:07 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('My Dummy Title'),
+      centerTitle: true,
+    ) ,
+    body: Center(
+      child: Image(
+        image: AssetImage('assets/2.jpg'),
+        fit: BoxFit.cover,
+        )
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+      },
+      child: Text('Gayatri '),
+    ),
+  );
+  }
+}
