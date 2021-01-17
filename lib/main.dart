f71491823110a642a1ace72eb1eba0910d72ea54
Yash <ydeshmukh0093@gmail.com>
2021-01-17 14:02:19 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Home extends StatelessWidget {
     floatingActionButton: FloatingActionButton(
       onPressed: () {
       },
-      child: Text('checkout'),
+      child: Text('Master'),
     ),
   );
   }
